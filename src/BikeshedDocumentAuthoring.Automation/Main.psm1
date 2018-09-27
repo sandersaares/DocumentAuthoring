2c7f779358d55a1b2cf2631b810dfff6ca0adbc9
Sander Saares <saares@axinom.com>
2018-09-27 07:52:05 +0300

    Build report is now updated after each build. Also with failed status.

--- src/BikeshedDocumentAuthoring.Automation/Main.psm1
+++ src/BikeshedDocumentAuthoring.Automation/Main.psm1
@@ -3,7 +3,8 @@ $ErrorActionPreference = "Stop"
 # Entry point of the module. Functions listed here can be exported to the user if declared in manifest.
 
 # Adds a comment to a GitHub pull request that contains links to the published output ready for review.
-function New-GitHubBuildReport() {
+# If a comment that looks like a build report already exists, it is edited instead.
+function New-SucceedingGitHubBuildReport() {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
@@ -28,7 +29,11 @@ function New-GitHubBuildReport() {
         [string]$oldHtmlUrl,
 
         [Parameter(Mandatory = $true)]
-        [string]$authorizationToken
+        [string]$authorizationToken,
+
+        # The username of the bot is used to look for an existing build report to edit (instead of posting a new one).
+        [Parameter(Mandatory = $true)]
+        [string]$botUsername
     )
 
     $url = "https://api.github.com/repos/$organization/$repository/issues/$issueNumber/comments"
@@ -37,10 +42,12 @@ function New-GitHubBuildReport() {
     $encodedOldHtmlUrl = [Uri]::EscapeDataString($oldHtmlUrl)
     $diffUrl = "https://services.w3.org/htmldiff?doc1=$encodedOldHtmlUrl&doc2=$encodedHtmlUrl"
 
-    $body += "Output from successful build:`r`n`r`n"
+    $body = $buildReportTag + "`r`n`r`n"
+    $body += "Build was successful. Outputs:`r`n`r`n"
     $body += "* [PDF document]($pdfUrl)`r`n"
     $body += "* [HTML document]($htmlUrl)`r`n"
-    $body += "* [HTML diff with target branch]($diffUrl)"
+    $body += "* [HTML diff with target branch]($diffUrl)`r`n`r`n"
+    $body += "This comment will be updated after each build."
 
     $requestBody = @{
         body = $body
@@ -50,8 +57,111 @@ function New-GitHubBuildReport() {
         Authorization = "token $authorizationToken"
     }
 
-    # PowerShell defaults to TLS 1.0 which is not really accepted these days.
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $existingBuildReportId = FindBuildReportCommentId -organization $organization -repository $repository -issueNumber $issueNumber -authorizationToken $authorizationToken -botUsername $botUsername
 
-    Invoke-WebRequest -Uri $url -UseBasicParsing -Method POST -Body $requestBody -ContentType "application/json" -Headers $requestHeaders
+    if ($existingBuildReportId) {
+        $url += "/$existingBuildReportId"
+
+        Invoke-WebRequest -Uri $url -UseBasicParsing -Method Patch -Body $requestBody -ContentType "application/json" -Headers $requestHeaders
+    }
+    else {
+        Invoke-WebRequest -Uri $url -UseBasicParsing -Method Post -Body $requestBody -ContentType "application/json" -Headers $requestHeaders
+    }
+}
+
+# Adds a comment to a GitHub pull request that informs the user of a failed build.
+# If a comment that looks like a build report already exists, it is edited instead.
+function New-FailingGitHubBuildReport() {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$organization,
+
+        [Parameter(Mandatory = $true)]
+        [string]$repository,
+
+        [Parameter(Mandatory = $true)]
+        [int]$issueNumber,
+
+        [Parameter(Mandatory = $true)]
+        [string]$authorizationToken,
+
+        # The username of the bot is used to look for an existing build report to edit (instead of posting a new one).
+        [Parameter(Mandatory = $true)]
+        [string]$botUsername
+    )
+
+    $url = "https://api.github.com/repos/$organization/$repository/issues/$issueNumber/comments"
+
+    $body = $buildReportTag + "`r`n`r`n"
+    $body += "Build failed. Outputs are not available if the build fails.`r`n`r`n"
+    $body += "This comment will be updated after each build."
+
+    $requestBody = @{
+        body = $body
+    } | ConvertTo-Json
+
+    $requestHeaders = @{
+        Authorization = "token $authorizationToken"
+    }
+
+    $existingBuildReportId = FindBuildReportCommentId -organization $organization -repository $repository -issueNumber $issueNumber -authorizationToken $authorizationToken -botUsername $botUsername
+
+    if ($existingBuildReportId) {
+        $url += "/$existingBuildReportId"
+
+        Invoke-WebRequest -Uri $url -UseBasicParsing -Method Patch -Body $requestBody -ContentType "application/json" -Headers $requestHeaders
+    }
+    else {
+        Invoke-WebRequest -Uri $url -UseBasicParsing -Method Post -Body $requestBody -ContentType "application/json" -Headers $requestHeaders
+    }
+}
+
+### Below this line is internal logic not exported to user. ###
+
+# PowerShell defaults to TLS 1.0 which is not really accepted these days.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# A post by the bot user that starts with this string is considered a build report.
+$buildReportTag = "# Automated build report"
+
+function FindBuildReportCommentId() {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$organization,
+
+        [Parameter(Mandatory = $true)]
+        [string]$repository,
+
+        [Parameter(Mandatory = $true)]
+        [int]$issueNumber,
+
+        [Parameter(Mandatory = $true)]
+        [string]$authorizationToken,
+
+        # The username of the bot is used to look for an existing build report to edit (instead of posting a new one).
+        [Parameter(Mandatory = $true)]
+        [string]$botUsername
+    )
+
+    # We only look on the first page (however many GitHub returns to us) for simplicity.
+    # The assumption is that the first build should have made a comment pretty quickly,
+    # so any existing build report should always be among the first comments.
+    $url = "https://api.github.com/repos/$organization/$repository/issues/$issueNumber/comments"
+
+    $requestHeaders = @{
+        Authorization = "token $authorizationToken"
+    }
+
+    $comments = (Invoke-WebRequest -Uri $url -UseBasicParsing -Headers $requestHeaders).Contents | ConvertFrom-Json
+
+    $buildReport = $comments | ? { $_.user.login -eq $botUsername -and $_.body -like "$buildReportTag*" } | Select-Object -First 1
+
+    if ($buildReport) {
+        return $buildReport.id
+    }
+    else {
+        return $null
+    }
 }
